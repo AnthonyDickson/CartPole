8df87a60d35e9b29fcbaac9d17b72f01145b37b5
Anthony Dickson <eight0153@gmail.com>
2018-12-07 21:08:08 +1300

    fix bug in utils and add script for watching trained models

--- utils.py
+++ utils.py
@@ -1,10 +1,19 @@
 from datetime import datetime
 import os
+import re
 
 class Utils:
     @staticmethod
     def get_run_path(prefix=''):
-        if prefix[-1] != '/':
+        """Generate a path string in the format 'prefix/yyyy/mm/dd/run/nnn'.
+
+        Arguments:
+            prefix: the path to prepend to the run path.
+
+        Returns: a path string in the format 'prefix/yyyy/mm/dd/run/nnn' 
+                 where prefix is the parameter 'prefix', yyyy/mm/dd is the date, and nnn is the 3-digit zero-padded run number.
+        """
+        if len(prefix) > 0 and prefix[-1] != '/':
             prefix = prefix + '/'    
     
         now = datetime.now()
@@ -17,11 +26,12 @@ class Utils:
             path += '/001'
         else:
             run_number = 1
+            pattern = re.compile("[0-9]{3}")
 
             with os.scandir(path) as it:
                 for entry in it:
-                    if entry.is_dir() and entry.name == '{:03d}'.format(run_number):
-                        run_number += 1
+                    if entry.is_dir() and re.match(pattern, entry.name):
+                        run_number = max(run_number, int(entry.name) + 1)
 
             path += '/{:03d}'.format(run_number)
 
--- watch_trained_model.py
+++ watch_trained_model.py
@@ -1,45 +1,40 @@
 
 import os
+import argparse
 
 import gym
         
 from logger import Logger
 from agent import CartPoleAgent
 
+parser = argparse.ArgumentParser(description='Load and watch a previously trained model.')
+parser.add_argument('path', type=str, help='the path of the model that is to be loaded.')
+parser.add_argument('--num-episodes', type=int, default=100, help='num of episodes to playback.')
+
+args = parser.parse_args()
+
 logger = Logger(Logger.Verbosity.MINIMAL)
 env = gym.make('CartPole-v0')
-agent = CartPoleAgent(env.action_space, env.observation_space)
+agent = CartPoleAgent.load(args.path)
 
-for i_episode in range(200):
+for i_episode in range(args.num_episodes):
     observation = env.reset() 
     prev_observation = None
     prev_action = None
 
-    episode = '[Episode {}]'.format(i_episode)       
-    logger.log('observations', episode)
-    logger.log('rewards', episode)
-    logger.log('actions', episode)
-
     for t in range(200):
+        env.render()
+
         action = agent.get_action(observation)
 
         prev_observation = observation
         prev_action = action
         observation, reward, done, info = env.step(action)
 
-        agent.update(prev_observation, prev_action, reward, observation)
-
-        logger.log('observations', observation)
-        logger.log('rewards', reward)
-        logger.log('actions', action)
-
         if done:
             msg = "Episode {:02d} finished after {:02d} timesteps".format(i_episode, t+1)
-            logger.log('episode_info', msg)
             print(msg)
 
             break
 
 env.close()
-logger.write()
-agent.save()
